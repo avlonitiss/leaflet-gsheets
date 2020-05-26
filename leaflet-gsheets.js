813829f720ea4e5951b761b902a03328c0825b35
Internet of Spyrus <std138004@ac.eap.gr>
2020-05-26 22:23:33 +0300

    fork poly points from google drive

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -11,9 +11,10 @@ function init() {
   // these URLs come from Google Sheets 'shareable link' form
   // the first is the polygon layer and the second the points
   var polyURL =
-    "https://docs.google.com/spreadsheets/d/1EUFSaqi30b6oefK0YWWNDDOzwmCTTXlXkFHAc2QrUxM/edit?usp=sharing";
+    "https://docs.google.com/spreadsheets/d/1fxG5eA57Tf8yURf1SAGuM5LHJuKineOOX-Fu_twMgWc/edit?usp=sharing";
+
   var pointsURL =
-    "https://docs.google.com/spreadsheets/d/1kjJVPF0LyaiaDYF8z_x23UulGciGtBALQ1a1pK0coRM/edit?usp=sharing";
+    "https://docs.google.com/spreadsheets/d/16QNGgrt4ocmsIZ0KQuSlRduAWSU6HJeFcD3BM5dhv54/edit?usp=sharing";
 
   Tabletop.init({ key: polyURL, callback: addPolygons, simpleSheet: true });
   Tabletop.init({ key: pointsURL, callback: addPoints, simpleSheet: true }); // simpleSheet assumes there is only one table and automatically sends its data
